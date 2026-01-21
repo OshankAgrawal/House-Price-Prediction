6d88d177fef982153f6d4e56467c76794be66bae
OshankAgrawal <oshankagrawal@gmail.com>
2026-01-21 14:30:02 +0530

    sklearn fits into modular design

--- house_price_prediction/model.py
+++ house_price_prediction/model.py
@@ -1,2 +1,23 @@
+from sklearn.linear_model import LinearRegression
+
 class HousePriceModel:
-    print("Import is working successfully!")
+    print("Class is called")
+    def __init__(self):    
+        """
+            Initialize the House Price Prediction model.
+        """
+        self.model = LinearRegression()
+
+    def fit(self, x, y):
+        """
+            Train the model on features x and target y.
+        """
+        print("Model fit function called")
+        self.model.fit(x, y)
+
+    def predict(self, x):
+        """
+            Predict house prices for given features x.
+        """
+        print("Model predict function call")
+        return self.model.predict(x)
